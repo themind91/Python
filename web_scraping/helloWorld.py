2b8903c888996fcf196eee6b074dcecefece25c3
Victor Rocha <themind1991@gmail.com>
2017-05-01 16:12:51 -0300

    adding a new file

--- web_scraping/helloWorld.py
+++ web_scraping/helloWorld.py
@@ -1,1 +1,12 @@
-ECHO est  ativado.
+import urllib2 as urlDown
+
+def download(url):
+	print 'Downloading the url', url
+	try:
+		html = urlDown.urlopen(url).read()
+	except urlDown.URLError as e:
+		print 'Download error', e.reason
+		html = None
+	return html	
+
+print download('https://www.facebook.com')
