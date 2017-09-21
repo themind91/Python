1cb88dc388b495de244d48c3bd5d13d15f6b64e4
Victor Rocha <themind1991@gmail.com>
2017-09-20 22:13:22 -0300

    atualizando o projeto

--- Jenkins_API/jen_py.py
+++ Jenkins_API/jen_py.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import jenkins
+from lxml import etree
 
 
 class JenkinsManager:
@@ -13,15 +14,42 @@ class JenkinsManager:
 		except Exception as e:
 			print 'failure to connect %s' %e
 
+	def create_tag(self,template):
+		root = etree.XML(template)
+		try:
+
+			for i in root.findall('builders'):
+				builder = i
+
+			shell_step = etree.Element('hudson.tasks.Shell')
+			comando = etree.Element('command')
+			comando.text = "texto bb"
+			shell_step.append(comando)
+			builder.append(shell_step)
+
+			return etree.tostring(root)
+		except Exception as error:
+			print error
+
+	
+		
+
 	def create_job(self):
 		try:
 			with open('bb.xml','r') as f:
-				template = f.read().replace('COMANDO','vai fazer falta')
-			print self.server.create_job('Python_apilivinho', template)
-			'criado com sucesso'
+
+				template = f.read()
+				print template
+				novotemplate = self.create_tag(template)
+				print type(novotemplate)
+				print self.server.create_job('Python_hudson', novotemplate)
+				'criado com sucesso'
 		except Exception as e:
 			print " Falha ao criar JOB: %s "%e
 
+	
+
+	
 
 if __name__ == '__main__':
 	j = JenkinsManager()
